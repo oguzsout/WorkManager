0c4bc01a798adbeb9ef0edf5f62d35106cbd147e
Oguzhan Dogdu <oguzd494@gmail.com>
2021-11-08 20:53:36 +0300

    The Worker class is used. Example complete

--- app/src/main/java/com/oguzdogdu/workmanager/MainActivity.kt
+++ app/src/main/java/com/oguzdogdu/workmanager/MainActivity.kt
@@ -2,10 +2,47 @@ package com.oguzdogdu.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.work.*
+import com.oguzdogdu.workmanager.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.button.setOnClickListener {
+            myWorkManager()
+        }
     }
+
+
+    private fun myWorkManager() {
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(false)
+            .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+            .setRequiresCharging(false)
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+        val myRequest = PeriodicWorkRequest.Builder(
+            MyWorker::class.java,
+            15,
+            TimeUnit.MINUTES
+        ).setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "my_id",
+                ExistingPeriodicWorkPolicy.KEEP,
+                myRequest
+            )
+    }
+
+
 }
